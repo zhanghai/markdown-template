0b0d06d7c17637e1321568fc1a1169f8605474df
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-04-30 15:54:01 +0800

    Add highlight.

--- generate_template.sh
+++ generate_template.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -e "s:\\\$APOLLO\\\$:$(readlink -f apollo.css):" -e "s:\\\$PANGU\\\$:$(readlink -f pangu.js):" meta_template.md >template.md
+sed -e "s:\\\$APOLLO\\\$:$(readlink -f apollo.css):" -e "s:\\\$PANGU\\\$:$(readlink -f pangu.js):" -e "s:\\\$HIGHLIGHT\\\$:$(readlink -f highlight.min.js):" meta_template.md >template.md
--- meta_template.md
+++ meta_template.md
@@ -1,5 +1,10 @@
 <link rel="stylesheet" href="$APOLLO$">
 <script src="$PANGU$"></script>
 <script>window.onload = pangu.page_spacing;</script>
+<script src="$HIGHLIGHT$"></script>
+<script>
+hljs.configure({classPrefix: ''});
+hljs.initHighlightingOnLoad();
+</script>
 
 
