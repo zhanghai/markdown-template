75a91f16651872924cdb48d2e540fc4c887db411
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-10-21 16:33:33 +0800

    Change install target name.

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ if [[ -z "${templates_dir}" ]]; then
   echo Unable to locate TEMPLATES user directory >&2
   exit 1
 fi
-cp template.md "${templates_dir}"/Markdown.md
+cp template.md "${templates_dir}/Markdown 文档.md"
